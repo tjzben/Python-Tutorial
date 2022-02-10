c018ee1336006479a18ea6a01dacd352e405d1b3
Ben Tan <tjzben@gmail.com>
2022-02-10 17:49:37 +0800

    Getting started with Integers & Floats

---
+++ Integers & Floats/integers & floats.py
@@ -0,0 +1,57 @@
+# Arithmetic Operators
+x, y = 3 , 2
+
+# Addition
+print(x + y) # = 5
+# Substraction
+print(x - y) # = 1
+# Multiplication
+print(x * y) # = 6
+# Division
+print(x / y) # = 1.5
+# Integer division - no remainder
+print(x // y) # = 1
+# Modulus - remainder of the divison
+print(x % y) # = 1s
+# Negative value assignment
+print(-x) # = -3
+# Absolute number
+print(abs(-x)) # = 3
+# Cast integer value
+print(int( 3.9 )) # = 3
+# Cast floating value
+print(float( 3 )) # = 3.0
+# Exponential 
+print(x ** y) # = 9
+
+# Comparison Operators - returns True or False accordingly to the condition
+
+a, b = 10, 20
+# Equal to
+print(a == b)
+# Not equal to 
+print(a != b)
+# Greater than
+print(a > b)
+# Greater or equal to
+print(a >= b)
+# Less than
+print(a < b)
+# Less than or equal to
+print( a <= b)
+
+# Logical Operators
+
+c = True
+d = False
+
+# AND
+print('c and d is', x and y)
+
+# OR
+print('c and d is', x or y)
+
+# NOT - True if operand is false (complements the operand)
+print ('not c is', not c)
+
+
